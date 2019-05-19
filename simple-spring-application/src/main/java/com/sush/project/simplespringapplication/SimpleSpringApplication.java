5557fce22bec5a1b0c47f429857ac1651813109f
G-susmithaa <34615419+G-susmithaa@users.noreply.github.com>
2019-05-19 21:22:15 +0530

    Add files via upload

---
+++ simple-spring-application/src/main/java/com/sush/project/simplespringapplication/SimpleSpringApplication.java
@@ -0,0 +1,17 @@
+package com.sush.project.simplespringapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SimpleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringApplication.class, args);
+	}
+	
+	
+
+}
